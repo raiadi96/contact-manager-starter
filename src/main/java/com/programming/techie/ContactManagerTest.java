4b665bd7845e24de0b3c7a844dcb06dfb88a3924
Aditya Rai <kaditya1298@gmail.com>
2022-03-10 01:02:20 +0530

    JUnit5 Testing

---
+++ src/main/java/com/programming/techie/ContactManagerTest.java
@@ -0,0 +1,14 @@
+package com.programming.techie;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ContactManagerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
